77f92774abb40428d530f082c1beb03f62159ca8
pluto <1575197604@qq.com>
2023-06-09 23:54:54 +0800

    fux

--- src/views/detector/components/Introduce.vue
+++ src/views/detector/components/Introduce.vue
@@ -3,10 +3,10 @@
     <h2>目前仅支持csv/xlsx文件格式，内容要求如下：</h2>
     <div style="height: 280px;">
       <el-steps direction="vertical" :active="4" style="font-size: 20px">
-        <el-step title="1.第一行为特征名或标签名，随后每一行为一条数据" />
-        <el-step title="2.标签列应在最后一列，列名称为label" />
-        <el-step title="3.特征列应为1~n任意列，最大不超过42" />
-        <el-step title="4.标签列应在最后一列，列名称为label" />
+        <el-step title="第一行为特征名或标签名，随后每一行为一条数据" />
+        <el-step title="标签列应在最后一列，列名称为label" />
+        <el-step title="特征列应为1~n任意列，最大不超过42" />
+        <el-step title="标签列应在最后一列，列名称为label" />
       </el-steps>
     </div>
     <div style="margin-top: 10px">
